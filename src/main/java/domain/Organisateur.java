eaa333d1d1c7b91211a62ac131263335510cff6c
Jihenzemzem <interns@fedhubs.fr>
2025-02-05 08:35:31 +0100

    creation des classes

---
+++ src/main/java/domain/Organisateur.java
@@ -0,0 +1,4 @@
+package domain;
+
+public class Organisateur extends Utilisateur{
+}
---
+++ src/main/java/domain/Utilisateur.java
@@ -0,0 +1,67 @@
+package domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class Utilisateur {
+
+
+    private Long id;
+    private String nom;
+    private String prenom;
+    private String motdepasse;
+    private String email;
+
+    public Utilisateur(){
+
+    }
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getMotdepasse() {
+        return motdepasse;
+    }
+
+    public void setMotdepasse(String motdepasse) {
+        this.motdepasse = motdepasse;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+
+}
