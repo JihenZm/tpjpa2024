27aebd7f076960d7d0bd77d2de6975445d3c1092
@ddokumaciogl <dogukan.dokumacioglu@etudiant.univ-rennes.fr>
2025-04-28 23:25:11 +0200

    Changements pour que RestServer démarre correctement

--- Backend/src/main/java/CorsFilter.java
+++ Backend/src/main/java/CorsFilter.java
@@ -1,13 +1,24 @@
 package rest; // ← C’est ton package actuel ("rest")
 
 import java.io.IOException;
-import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.container.ContainerResponseContext;
-import javax.ws.rs.container.ContainerResponseFilter;
-import javax.ws.rs.ext.Provider;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+
 
 @Provider
-public class CorsFilter implements ContainerResponseFilter {
+public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        if (requestContext.getMethod().equalsIgnoreCase("OPTIONS")) {
+            requestContext.abortWith(
+                    jakarta.ws.rs.core.Response.ok().build()
+            );
+        }
+    }
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
