25195ed11f3665ee53ec237bc1830da59ac8fc84
amir-coder <ja_amir@esi.dz>
2025-04-30 16:10:58 +0200

    fixed date to string format

--- src/main/java/dto/EvenementDTO.java
+++ src/main/java/dto/EvenementDTO.java
@@ -1,16 +1,22 @@
 package dto;
 import domain.Evenement;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import java.time.format.DateTimeFormatter;
+import java.text.SimpleDateFormat;
+
+
 
 // EvenementDTO.java
 public class EvenementDTO {
     public Long id;
     public String titre;
 
-    public Date date;
+    public String date;
     public String description;
     public String lieu;
     public double prix;
@@ -18,16 +24,19 @@ public class EvenementDTO {
     public List<TicketDTO> tickets;
 
     public EvenementDTO(Evenement evenement) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         this.id = evenement.getId();
         this.titre = evenement.getTitre();
-        this.date = evenement.getDate();
+        this.date = sdf.format(evenement.getDate());
         this.description = evenement.getDescription();
         this.lieu = evenement.getLieu();
         this.prix = evenement.getPrix();
         this.popularite = evenement.getPopularite();
-        this.tickets = evenement.getTickets().stream()
-                .map(TicketDTO::new)
-                .collect(Collectors.toList());
+        this.tickets = evenement.getTickets() == null ? new ArrayList<>() :
+                evenement.getTickets().stream()
+                        .map(TicketDTO::new)
+                        .collect(Collectors.toList());
+
     }
 }
 
